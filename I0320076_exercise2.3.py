49b3b73ae504792d0d149832f31574dbb5e8a827
Nurki Putra Mahardika <nurput28@gmail.com>
2021-03-12 22:39:22 +0700

    Latihan 2.3

---
+++ I0320076_exercise2.3.py
@@ -0,0 +1,17 @@
+import math
+
+#Menampilkan informasi program
+print("Luas dan Keliling Lingkaran")
+
+#Input nilai hari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+#Menghitung luas lingkaran
+Luas_lingkaran = 3.14 * (r**2)
+
+#Menghitung keliling lingkaran
+Keliling_Lingkaran = 2*3.14*r
+
+#Menampilkan hasil perhitungan kelayar
+print("Luas lingkaran: ", Luas_lingkaran)
+print("Keliling Lingkaran: ", Keliling_Lingkaran)
