08b0dd162e2ecc6c623582c9783ab5fb2f34874e
Nurki Putra Mahardika <nurput28@gmail.com>
2021-03-12 16:58:05 +0700

    Tugas 2 Nomor 2

---
+++ I0320076_soal2_tugas2.py
@@ -0,0 +1,40 @@
+print('====================================')
+print('Identitas Diri dan Perkenalan')
+print('====================================')
+
+data_stringnama = 'Nurki Putra Mahardika'
+print('Halo, Nama saya', data_stringnama)
+data_integerumur = 18
+print('Saya sekarang berumur ', data_integerumur, 'tahun')
+data_integerbulan = ((data_integerumur*12)+10)
+print('Jika dalam bulan umur saya adalah', data_integerbulan, 'bulan')
+data_umurhari = (data_integerbulan*30)
+print('Umur saya dalam hari kira-kira adalah', data_umurhari, 'hari')
+data_RT = 1
+data_RW = 13
+data_Kelurahan = 'Mojosongo'
+data_Kecamatan = 'Jebres'
+data_kota = 'Surakarta'
+print('alamat lengkap rumah saya adalah RT', data_RT, 'RW', data_RW, data_Kelurahan, data_Kecamatan, data_kota)
+data_ukuransepatu = 41
+data_beratbadan = 65
+data_tinggibadan = 161
+print('ukuran sepatu saya adalah', data_ukuransepatu)
+print('Sedangkan, berat badan saya adalah', data_beratbadan, 'kg dan tinggi badan saya adalah', data_tinggibadan, 'cm')
+data_hobi = 'Menulis cerpen dan membaca buku'
+data_bukufavorit = 'Gun, Germs, and Steel'
+data_pengarang = 'Jared Diamond'
+data_idola = 'Tere Liye'
+print('Hobi saya adalah', data_hobi)
+print('Buku paling berkesan yang saya baca adalah buku dengan judul', data_bukufavorit, 'karangan', data_pengarang)
+print('Pengarang buku yang saya idolakan adalah', data_idola, 'dengan berbagai seri bukunya')
+
+
+data_SMA = 'SMAN 1 Surakarta'
+data_float_jarak = 3.5
+data_jurusan = 'Teknik Industri'
+data_universitas = 'Universitas Sebelas Maret'
+print('saya bersekolah di',data_SMA ,'yang berjarak', data_float_jarak, 'km dari rumah')
+print('setelah lulus SMA, saya melanjutkan studi di jurusan', data_jurusan, data_universitas)
+print('------------------------------------------------------------------------------------------------')
+print('sekian perkenalan dari saya, salam kenal!')
